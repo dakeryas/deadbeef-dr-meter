8bdf491bf360d938f5daf167be1038191c924ec9
vsibille <vsibille@mit.edu>
2020-05-16 23:39:14 +0200

    DR seems not use histograms; use raw samples squared and second peak instead

--- include/dr_meter.h
+++ include/dr_meter.h
@@ -1,21 +1,23 @@
 #ifndef DR_METER_H
 #define DR_METER_H
 
-#include "histogram.h"
-
 struct block_analyser_s;
 
 typedef struct
 {
-    double min_hist;
-    double max_hist;
     unsigned channels;
-    histogram_t* peaks_hist;
-    histogram_t* rms_hist;
+    unsigned blocks; // expected blocks to analyse
+    double* peaks; // maximum per channel
+    double* second_peaks; // second maximum per channel
+    double** sum2; // sum of squared sample values per block per channel
+    unsigned _ana_blocks; // analysed blocks
+    unsigned _ana_samples; // analysed samples
 } dr_meter_t;
 
 void init_dr_meter(dr_meter_t* dr_meter);
 void fill_dr_meter(dr_meter_t* dr_meter, struct block_analyser_s* analyser);
+double get_rms_dr_meter(dr_meter_t* dr_meter, unsigned channel);
+double get_dr_dr_meter(dr_meter_t* dr_meter, unsigned channel);
 void free_dr_meter(dr_meter_t* dr_meter);
 
 #endif /* DR_METER_H */
--- src/dr_meter.c
+++ src/dr_meter.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include "dr_meter.h"
+#include "audio_rms.h"
 #include "block_analyser.h"
 
 static double decibels(double value)
@@ -11,42 +12,99 @@ static double decibels(double value)
 
 void init_dr_meter(dr_meter_t* dr_meter)
 {
-    dr_meter->peaks_hist = malloc(dr_meter->channels * sizeof(histogram_t));
-    dr_meter->rms_hist = malloc(dr_meter->channels * sizeof(histogram_t));
-    for(unsigned channel_index = 0; channel_index < dr_meter->channels ; ++channel_index)
-    {
-        init_hist_mm(&dr_meter->peaks_hist[channel_index], dr_meter->min_hist, dr_meter->max_hist);
-        init_hist_mm(&dr_meter->rms_hist[channel_index], dr_meter->min_hist, dr_meter->max_hist);
-    }
+    dr_meter->_ana_blocks = 0;
+    dr_meter->_ana_samples = 0;
+    dr_meter->peaks = calloc(dr_meter->channels, sizeof(double));
+    dr_meter->second_peaks = calloc(dr_meter->channels, sizeof(double));
+    dr_meter->sum2 = malloc(dr_meter->channels * sizeof(*dr_meter->sum2));// allocate dr_meter->channels pointers
+    for(unsigned cha = 0; cha < dr_meter->channels; ++cha)
+        dr_meter->sum2[cha] = malloc(dr_meter->blocks * sizeof(double));//allocate dr_meter->blocks 1D arrays
 }
 
 static void fill_dr_peaks(dr_meter_t* dr_meter, block_analyser_t* analyser, unsigned channel_index)
 {
-    float peak_dB =  decibels(analyser->peak[channel_index]);
-    fill_hist(&dr_meter->peaks_hist[channel_index], peak_dB);
+    double block_peak = analyser->peak[channel_index];
+    double* first = &dr_meter->peaks[channel_index];
+    double* second = &dr_meter->second_peaks[channel_index];
+    if(block_peak > *first)
+    {
+        *second = *first;
+        *first = block_peak;
+    }
+    else if(block_peak > *second) *second = block_peak;
 }
 
-static void fill_dr_rms(dr_meter_t* dr_meter, block_analyser_t* analyser, unsigned channel_index)
+static void fill_sum2(dr_meter_t* dr_meter, block_analyser_t* analyser, unsigned channel_index)
 {
-    float rms_dB = decibels(get_analyser_rms(analyser, channel_index));
-    fill_hist(&dr_meter->rms_hist[channel_index], rms_dB);
+    dr_meter->sum2[channel_index][dr_meter->_ana_blocks] = analyser->sum2[channel_index];
 }
 
 static void fill_dr_meter_channel(dr_meter_t* dr_meter, block_analyser_t* analyser, unsigned channel_index)
 {
     fill_dr_peaks(dr_meter, analyser, channel_index);
-    fill_dr_rms(dr_meter, analyser, channel_index);
+    fill_sum2(dr_meter, analyser, channel_index);
 }
 
 void fill_dr_meter(dr_meter_t* dr_meter, block_analyser_t* analyser)
 {
+    assert((dr_meter->_ana_blocks < dr_meter->blocks));
     assert((analyser->channels == dr_meter->channels));
     for(unsigned channel_index = 0; channel_index < dr_meter->channels; ++channel_index)
         fill_dr_meter_channel(dr_meter, analyser, channel_index);
+    dr_meter->_ana_samples += analyser->samples;
+    ++dr_meter->_ana_blocks;
+}
+
+static int reverse_comp_samples(const void* s1, const void* s2)
+{
+
+    double diff = *(const double*)s1 - *(const double*)s2;//abs(diff) may be smaller than 1
+    if(diff > 0) return -1;
+    else if(diff < 0) return 1;
+    else return 0;
+}
+
+static void sort_sum2(dr_meter_t* dr_meter, unsigned channel)
+{
+    qsort(dr_meter->sum2[channel], dr_meter->_ana_blocks, sizeof(double), reverse_comp_samples);
+}
+
+static void sort_all_sum2(dr_meter_t* dr_meter)
+{
+    for(unsigned channel_index = 0; channel_index < dr_meter->channels; ++channel_index)
+        sort_sum2(dr_meter, channel_index);
+}
+
+static double get_sum2_dr_meter(dr_meter_t* dr_meter, unsigned channel, unsigned last_block)
+{
+    double sum2 = 0.;
+    for(unsigned block_index = 0; block_index < last_block; ++block_index)
+        sum2 += dr_meter->sum2[channel][block_index];
+    return sum2;
+}
+
+static double get_tot_sum2_dr_meter(dr_meter_t* dr_meter, unsigned channel)
+{
+    return get_sum2_dr_meter(dr_meter, channel, dr_meter->_ana_blocks);
+}
+
+double get_rms_dr_meter(dr_meter_t* dr_meter, unsigned channel)
+{
+    double sum2 = get_tot_sum2_dr_meter(dr_meter, channel);
+    return get_audio_rms(sum2, dr_meter->_ana_samples);
+}
+
+double get_dr_dr_meter(dr_meter_t* dr_meter, unsigned channel)
+{
+    sort_sum2(dr_meter, channel);
+    unsigned last_block = 0.2 * dr_meter->_ana_blocks; //loudest 20%
+    double loudest_20_rms = sqrt(get_sum2_dr_meter(dr_meter, channel, last_block) / (0.2 * dr_meter->_ana_samples));
+    return decibels(dr_meter->second_peaks[channel] / loudest_20_rms);
 }
 
 void free_dr_meter(dr_meter_t* dr_meter)
 {
-    free(dr_meter->peaks_hist);
-    free(dr_meter->rms_hist);
+    free(dr_meter->peaks);
+    free(dr_meter->second_peaks);
+    free(dr_meter->sum2);
 }
