edddddce4b5ac546fc7b3dc539eb5bc3df123963
vsibille <vsibille@mit.edu>
2020-06-06 17:22:15 +0200

    Use 'this' in block_analyser member functions

--- dr_plugin/src/block_analyser.c
+++ dr_plugin/src/block_analyser.c
@@ -5,15 +5,15 @@
 #include "audio_rms.h"
 #include "channel_interpreter.h"
 
-static unsigned size_analyser_array(block_analyser_t* analyser)
+static unsigned size_analyser_array(block_analyser_t* this)
 {
-    return analyser->channels * sizeof(block_res);
+    return this->channels * sizeof(block_res);
 }
 
-static void init_block_analyser(block_analyser_t* analyser)
+static void init_block_analyser(block_analyser_t* this)
 {
-    analyser->peak = malloc(size_analyser_array(analyser));
-    analyser->sum2 = malloc(size_analyser_array(analyser));
+    this->peak = malloc(size_analyser_array(this));
+    this->sum2 = malloc(size_analyser_array(this));
 }
 
 block_analyser_t make_block_analyser(unsigned channels)
@@ -23,44 +23,44 @@ block_analyser_t make_block_analyser(unsigned channels)
     return analyser;
 }
 
-static void reset_block_analyser(block_analyser_t* analyser)
+static void reset_block_analyser(block_analyser_t* this)
 {
-    memset(analyser->peak, 0., size_analyser_array(analyser));
-    memset(analyser->sum2, 0., size_analyser_array(analyser));
-    analyser->samples = 0;
+    memset(this->peak, 0., size_analyser_array(this));
+    memset(this->sum2, 0., size_analyser_array(this));
+    this->samples = 0;
 }
 
-static void update_peak(block_analyser_t* analyser, double sample_value, int channel_index)
+static void update_peak(block_analyser_t* this, double sample_value, int channel_index)
 {
     double value = fabs(sample_value);
-    if(value >analyser->peak[channel_index]) analyser->peak[channel_index] = value;
+    if(value >this->peak[channel_index]) this->peak[channel_index] = value;
 }
 
-static void update_moment2(block_analyser_t* analyser, double sample_value, int channel_index)
+static void update_moment2(block_analyser_t* this, double sample_value, int channel_index)
 {
-    analyser->sum2[channel_index] += sample_value * sample_value;
+    this->sum2[channel_index] += sample_value * sample_value;
 }
 
-static void analyse_channel_value(block_analyser_t* analyser, double sample_value, int channel_index)
+static void analyse_channel_value(block_analyser_t* this, double sample_value, int channel_index)
 {
-    update_peak(analyser, sample_value, channel_index);
-    update_moment2(analyser, sample_value, channel_index);
+    update_peak(this, sample_value, channel_index);
+    update_moment2(this, sample_value, channel_index);
 }
 
-static void analyse_sample(block_analyser_t* analyser, char* sample_begin, int nu_channels, int bytes_per_channel)
+static void analyse_sample(block_analyser_t* this, char* sample_begin, int nu_channels, int bytes_per_channel)
 {
     for(int channel_index = 0; channel_index < nu_channels; ++channel_index)
     {
         char* channel_begin = sample_begin + channel_index * bytes_per_channel;
         double channel_value = get_normalised_channel_value(channel_begin, bytes_per_channel);
-        analyse_channel_value(analyser, channel_value, channel_index);
+        analyse_channel_value(this, channel_value, channel_index);
     }
-    ++analyser->samples;
+    ++this->samples;
 }
 
-void analyse_block(block_analyser_t* analyser, char* bytes_begin, unsigned buffer_size, int nu_channels, int bits_per_channel, int sample_rate)
+void analyse_block(block_analyser_t* this, char* bytes_begin, unsigned buffer_size, int nu_channels, int bits_per_channel, int sample_rate)
 {
-    reset_block_analyser(analyser);
+    reset_block_analyser(this);
     int bytes_per_channel = bits_per_channel >> 3;
     int bytes_per_sample = nu_channels * bytes_per_channel;
     unsigned nu_samples = buffer_size / bytes_per_sample;
@@ -68,24 +68,24 @@ void analyse_block(block_analyser_t* analyser, char* bytes_begin, unsigned buffe
     for(size_t sample_index = 0; sample_index < nu_samples; ++sample_index)
     {
         char* sample_begin = bytes_begin + sample_index * bytes_per_sample;
-        analyse_sample(analyser, sample_begin, nu_channels, bytes_per_channel);
+        analyse_sample(this, sample_begin, nu_channels, bytes_per_channel);
     }
 }
 
-double get_rms_analyser(block_analyser_t* analyser, unsigned channel)
+double get_rms_analyser(block_analyser_t* this, unsigned channel)
 {
-    return get_audio_rms(analyser->sum2[channel], analyser->samples);
+    return get_audio_rms(this->sum2[channel], this->samples);
 }
 
-void free_block_analyser(block_analyser_t* analyser)
+void free_block_analyser(block_analyser_t* this)
 {
-    free(analyser->peak);
-    free(analyser->sum2);
+    free(this->peak);
+    free(this->sum2);
 }
 
-void  print_block_analyser(block_analyser_t* analyser, FILE* output)
+void print_block_analyser(block_analyser_t* this, FILE* output)
 {
-    for(unsigned cha = 0; cha < analyser->channels; ++cha)
-        fprintf(output, "ch%i: %f %f\t", cha, analyser->peak[cha], get_rms_analyser(analyser, cha));
-    fprintf(output, "%i sp\n", analyser->samples);
+    for(unsigned cha = 0; cha < this->channels; ++cha)
+        fprintf(output, "ch%i: %f %f\t", cha, this->peak[cha], get_rms_analyser(this, cha));
+    fprintf(output, "%i sp\n", this->samples);
 }
