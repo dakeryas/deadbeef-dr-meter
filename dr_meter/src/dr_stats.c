71d47e2e83264a442f58299a0e2f324adc54edd2
vsibille <vsibille@mit.edu>
2020-06-07 21:06:41 +0200

    Two digits in DR_FORMAT and add printl_dr_stats with newline

--- dr_meter/src/dr_stats.c
+++ dr_meter/src/dr_stats.c
@@ -2,7 +2,7 @@
 #include "math.h"
 #include "decibels.h"
 
-static const char* DR_FORMAT = "DR %-8.3f %8.3f dB %10.3f dB";
+static const char* DR_FORMAT = "DR %-8.3f %7.2f dB %9.2f dB";
 
 dr_stats_t make_dr_stats(double dr, double peak, double rms)
 {
@@ -34,8 +34,15 @@ int print_buffer_dr_stats(dr_stats_t* dr_stats, char* buffer)
     dr_stats_t stats = make_dB_dr_stats(dr_stats);
     return sprintf(buffer, DR_FORMAT, stats.dr, stats.peak, stats.rms);
 }
+
 int print_dr_stats(dr_stats_t* dr_stats, FILE* output)
 {
     dr_stats_t stats = make_dB_dr_stats(dr_stats);
     return fprintf(output, DR_FORMAT, stats.dr, stats.peak, stats.rms);
+}
+
+int printl_dr_stats(dr_stats_t* dr_stats, FILE* output)
+{
+    int written_bytes = print_dr_stats(dr_stats, output);
+    return written_bytes + fprintf(output, "\n");
 }
