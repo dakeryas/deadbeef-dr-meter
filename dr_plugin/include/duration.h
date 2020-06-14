85c5033b9418f2d6244d585886ffed4211f03303
vsibille <vsibille@mit.edu>
2020-06-14 16:41:26 +0200

    Add duration_t

---
+++ dr_plugin/include/duration.h
@@ -0,0 +1,23 @@
+/**************************************************************************
+* @file duration.h
+* @author Valérian Sibille <vsibille@mit.edu>
+* @date 14 Jun 2020
+* @brief struct to split time in seconds into hours, minutes, seconds
+* ************************************************************************/
+
+#ifndef DURATION_H
+#define DURATION_H
+
+struct duration_s
+{
+    unsigned hours;
+    unsigned minutes;
+    unsigned seconds;
+};
+
+typedef struct duration_s duration_t;
+
+duration_t make_duration(double length_seconds);
+int print_buffer_duration(duration_t* dur, char* buffer);
+
+#endif /* DURATION_H */
---
+++ dr_plugin/src/duration.c
@@ -0,0 +1,18 @@
+#include "duration.h"
+#include <math.h>
+#include  <stdio.h>
+
+duration_t make_duration(double track_length)
+{
+    unsigned length = rint(track_length);
+    duration_t duration =
+    {.hours = length / 3600, .minutes = (length % 3600) / 60, .seconds = (length % 3600) % 60};
+    return duration;
+}
+
+int print_buffer_duration(duration_t* dur, char* buffer)
+{
+    char* format = "%.0s%2d:%02d";
+    if(dur->hours) format = "%2d:%02d:%02d";
+    return sprintf(buffer, format, dur->hours, dur->minutes, dur->seconds);
+}
