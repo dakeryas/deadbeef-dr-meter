6ea9e27b0cd2ae43abed37fd8d3e0586cb05cc74
vsibille <vsibille@mit.edu>
2020-06-22 02:32:01 +0200

    Increase track title column to 80 bytes (40 UTF8 characters)

--- dr_plugin/src/dr_meter_plugin.c
+++ dr_plugin/src/dr_meter_plugin.c
@@ -116,7 +116,7 @@ static unsigned sprint_track_info(void* track, char* begin)
     ddb_api->pl_lock();
     const char* title = ddb_api->pl_find_meta_raw(item, "title");
     ddb_api->pl_unlock();
-    end += sprintf(end, "  %02d-%-.60s", track_number, title);
+    end += sprintf(end, "  %02d-%-.80s", track_number, title);
     return end - begin;
 }
 
