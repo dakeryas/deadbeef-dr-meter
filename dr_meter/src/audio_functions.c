ca645b04be67457ffe2e50e91a2f7d3c91035b55
vsibille <vsibille@mit.edu>
2021-08-10 11:47:29 +0200

    Fix typo

--- dr_meter/src/audio_functions.c
+++ dr_meter/src/audio_functions.c
@@ -13,5 +13,5 @@ double get_audio_rms(double sum2, unsigned number_of_samples)
 
 double true_peak(double peak_value)
 {
-    return fabs(peak_values) < 1. ? peak_value : (peak_value < 0 ? -1. : 1.);
+    return fabs(peak_value) < 1. ? peak_value : (peak_value < 0 ? -1. : 1.);
 }
