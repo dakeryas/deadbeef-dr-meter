b688fc4158b1271698e8b4797b5c6e737c1b2c48
vsibille <vsibille@mit.edu>
2021-11-17 15:26:12 -0500

    Update sprint_dr_log const signature

--- dr_plugin/include/dr_meter_plugin.h
+++ dr_plugin/include/dr_meter_plugin.h
@@ -16,7 +16,7 @@ typedef struct
 {
     DB_misc_t info;
     int (*compute_dr)(struct thread_data_s* thread_data, unsigned max_threads);
-    unsigned (*sprint_dr_log)(struct thread_data_s* thread_data, char* buffer);
+    unsigned (*sprint_dr_log)(const struct thread_data_s* thread_data, char* buffer);
 } dr_meter_plugin_t;
 
 #endif /* DR_METER_PLUGIN_H */
