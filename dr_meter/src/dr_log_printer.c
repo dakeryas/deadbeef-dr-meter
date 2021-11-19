7dd327de7952eede11ffa34bc5db953809b44ae7
vsibille <vsibille@mit.edu>
2021-11-19 18:10:58 -0500

    Print album info header and footer whenever track is from a new album

--- dr_meter/src/dr_log_printer.c
+++ dr_meter/src/dr_log_printer.c
@@ -1,5 +1,6 @@
 #include "dr_log_printer.h"
 #include "tagged_dr_data.h"
+#include "sample_mean.h"
 
 static unsigned sprint_n(char character, unsigned count, char* begin, char endline)
 {
@@ -41,18 +42,17 @@ unsigned sprintl_item_dr_log_printer(const dr_log_printer_t* self, tagged_dr_dat
     return sprint_item_dr_log_printer(self, datum, begin, '\n');
 }
 
-static unsigned sprint_album_info(const dr_log_printer_t* self, const tagged_dr_data_t* data, char* begin, char endline)
+static unsigned sprint_album_info(const dr_log_printer_t* self, void* track, char* begin, char endline)
 {
     char* end = begin;
-    void* first_track = data->data[0].item;
-    end += self->sprint_album_info(first_track, end);
+    end += self->sprint_album_info(track, end);
     end += sprintf(end, "%c", endline);
     return end - begin;
 }
 
-static unsigned sprintl_album_info(const dr_log_printer_t* self, const tagged_dr_data_t* data, char* begin)
+static unsigned sprintl_album_info(const dr_log_printer_t* self, void* track, char* begin)
 {
-    return sprint_album_info(self, data, begin, '\n');
+    return sprint_album_info(self, track, begin, '\n');
 }
 
 unsigned sprint_col_headers_dr_log_printer(const dr_log_printer_t* self, char* begin, char endline)
@@ -64,6 +64,18 @@ unsigned sprint_col_headers_dr_log_printer(const dr_log_printer_t* self, char* b
 unsigned sprintl_col_headers_dr_log_printer(const dr_log_printer_t* self, char* begin)
 {
     return sprint_col_headers_dr_log_printer(self, begin, '\n');
+}
+
+unsigned sprintl_album_header_dr_log_printer(const dr_log_printer_t* self, void* track, char* begin)
+{
+    char* end = begin;
+    end += sprintl_line(self->line_length, end);
+    end += sprintl_album_info(self, track, end);
+    end += sprintl_line(self->line_length, end);
+    end += sprintl_line(0, end);
+    end += sprintl_col_headers_dr_log_printer(self, end);
+    end += sprintl_line(self->line_length, end);
+    return end - begin;
 }
 
 unsigned sprint_summary_info(const dr_log_printer_t* self, unsigned items, double avg_dr, char* begin, char endline)
@@ -80,27 +92,36 @@ unsigned sprintl_summary_info(const dr_log_printer_t* self, unsigned items, doub
     return sprint_summary_info(self, items, avg_dr, begin, '\n');
 }
 
+unsigned sprintl_album_footer_dr_log_printer(const dr_log_printer_t* self, sample_mean_t* dr_mean, char* begin)
+{
+    char* end = begin;
+    end += sprintl_line(self->line_length, end);
+    end += sprintl_line(0, end);
+    end += sprintl_summary_info(self, dr_mean->samples, get_sample_mean(dr_mean), end);
+    end += sprintl_n('=', self->line_length, end);
+    return end - begin;
+}
+
 unsigned sprint_log_dr_log_printer(const dr_log_printer_t* self, const tagged_dr_data_t* tagged_dr_data, char* begin)
 {
     char* end = begin;
-    unsigned line_length = 80;
-    end += sprintl_line(line_length, end);
-    end += sprintl_album_info(self, tagged_dr_data, end);
-    end += sprintl_line(line_length, end);
-    end += sprintl_line(0, end);
-    end += sprintl_col_headers_dr_log_printer(self, end);
-    end += sprintl_line(line_length, end);
-    double avg_dr = 0.;
+    sample_mean_t dr_mean = make_sample_mean();
+    void* last_track = tagged_dr_data->data->item;
+    end += sprintl_album_header_dr_log_printer(self, last_track, end);
     for(unsigned k = 0; k < tagged_dr_data->items; ++k)
     {
         tagged_dr_datum_t* datum = tagged_dr_data->data + k;
-        avg_dr += datum->dr_stats.dr;
+        int new_album = !self->same_album(last_track, datum->item);
+        if(new_album)
+        {
+            end += sprintl_album_footer_dr_log_printer(self, &dr_mean, end);
+            reset_sample_mean(&dr_mean);
+            end += sprintl_album_header_dr_log_printer(self, datum->item, end);
+        }
         end += sprintl_item_dr_log_printer(self, datum, end);
+        observe_sample_mean(&dr_mean, datum->dr_stats.dr);
+        last_track = datum->item;
     }
-    avg_dr /= tagged_dr_data->items;
-    end += sprintl_line(line_length, end);
-    end += sprintl_line(0, end);
-    end += sprintl_summary_info(self, tagged_dr_data->items, avg_dr, end);
-    end += sprintl_n('=', line_length, end);
+    end += sprintl_album_footer_dr_log_printer(self, &dr_mean, end);
     return end - begin;
 }
