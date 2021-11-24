9df35742744c21dc9dacf0f710c609f02803edd1
vsibille <vsibille@mit.edu>
2021-11-24 10:19:01 -0500

    Simplify gtk_css_provider_load_from_data call

--- dr_plugin_gui/src/mono_text.c
+++ dr_plugin_gui/src/mono_text.c
@@ -6,8 +6,7 @@ static void set_monospace(GtkTextView* text)
 #if GTK_CHECK_VERSION(3,0,0)
     GtkStyleContext *context = gtk_widget_get_style_context(GTK_WIDGET(text));
     GtkCssProvider *provider = gtk_css_provider_new();
-    char css[] = "textview {font-family: \"monospace\";}";
-    gtk_css_provider_load_from_data(provider, css, sizeof(css), NULL);
+    gtk_css_provider_load_from_data(provider, "textview {font-family: \"monospace\";}", -1, NULL);
     gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 #else
     PangoFontDescription *descr = pango_font_description_from_string("monospace");
